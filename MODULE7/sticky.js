f1d422d6b6c7f1054d9622e08cdb8d8ee9936e31
unknown <jeannabong@rocketmail.com>
2013-08-24 17:57:43 -0700

    renamed the folder MODULE7 to module-seven

--- MODULE7/sticky.js
+++
@@ -1,9 +0,0 @@
-	var sticky = document.querySelector('.sticky');
-	var origOffsetY = sticky.offsetTop;
-
-	function onScroll(e) {
-  		window.scrollY >= origOffsetY ? sticky.classList.add('fixed') :
-                                  sticky.classList.remove('fixed');
-	}
-
-	document.addEventListener('scroll', onScroll);
